95c2246f3a206c00429159aa8faeb3a071a25b2d
dashque <zagorkiynahyi@gmail.com>
2026-07-06 14:06:14 +0300

    feat: add error handling to profile form fields

--- src/app/features/profile/ui/profile-form.component.ts
+++ src/app/features/profile/ui/profile-form.component.ts
@@ -4,6 +4,7 @@ import { TuiPassword } from '@taiga-ui/kit';
 import { TranslocoPipe } from '@jsverse/transloco';
 import {
   TuiButton,
+  TuiError,
   TuiIcon,
   TuiInputDirective,
   TuiLabel,
@@ -13,7 +14,6 @@ import {
 } from '@taiga-ui/core';
 import { TuiCardLarge, TuiForm } from '@taiga-ui/layout';
 import { ReactiveFormsModule } from '@angular/forms';
-import { PASSWORD_MAX_LENGTH, PASSWORD_MIN_LENGTH } from '@shared/constants/password-length';
 
 @Component({
   selector: 'ngKitty-profile-form',
@@ -29,20 +29,16 @@ import { PASSWORD_MAX_LENGTH, PASSWORD_MIN_LENGTH } from '@shared/constants/pass
     TuiForm,
     TuiTextfieldComponent,
     ReactiveFormsModule,
+    TuiError,
   ],
   providers: [tuiLoaderOptionsProvider({ size: 'm' })],
   templateUrl: './profile-form.component.html',
   styleUrl: './profile-form.component.scss',
-  standalone: true,
 })
 export class ProfileFormComponent {
   private readonly profileFacade = inject(ProfileFacade);
   protected readonly isLoading = this.profileFacade.isLoading;
   protected readonly form = this.profileFacade.profileForm;
-  protected readonly passwordHints = {
-    minLength: PASSWORD_MIN_LENGTH,
-    maxLength: PASSWORD_MAX_LENGTH,
-  } as const;
 
   protected onSubmit(event: SubmitEvent): void {
     event.preventDefault();
