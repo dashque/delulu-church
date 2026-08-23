b4194569a24b4503194e9dbe4ffeb1ba377b7cec
dashque <zagorkiynahyi@gmail.com>
2026-08-23 14:38:45 +0300

    refactor(profile): update form model structure and enhance `ProfileFormService` logic

---
+++ src/app/features/profile/services/profile-form/profile-form.service.mock.ts
@@ -0,0 +1,11 @@
+import type { MockedObject } from 'vitest';
+import type { ProfileFormService } from '@features/profile/services/profile-form/profile-form.service';
+import { profileFormFixture } from '@features/profile/fixtures/profile-form.fixture';
+import { EMPTY } from 'rxjs';
+
+export const profileFormServiceMock = {
+  form: profileFormFixture,
+  get formValueChanges() {
+    return EMPTY;
+  },
+} as const satisfies MockedObject<Partial<ProfileFormService>>;
