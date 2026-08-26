53f3396adfed54cb9e182c72e634e21ee5f7755f
tryproxy <tryproxy@protonmail.com>
2026-08-26 10:49:59 +0400

    refactor(features/main-page): use rxResource states handling and loding data

--- src/app/features/main/facades/main-page.facade.ts
+++ src/app/features/main/facades/main-page.facade.ts
@@ -1,11 +1,10 @@
-import { computed, DestroyRef, effect, inject, linkedSignal, Service, signal } from '@angular/core';
+import { computed, effect, inject, linkedSignal, Service, signal } from '@angular/core';
 import { TarotService } from '@features/main/data/api/services/tarot/tarot.service';
 import type { TarotResponseApi } from '@features/main/data/api/models/deploy-tarot-response-api.model';
 import { finalize } from 'rxjs';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { rxResource } from '@angular/core/rxjs-interop';
 import { MyMemoryTranslationService } from '@features/main/data/api/services/my-memory-translation/my-memory-translation.service';
 import { TranslocoService } from '@jsverse/transloco';
-import { HttpErrorResponse } from '@angular/common/http';
 import type { Languages } from '@core/models/languages.model';
 
 @Service({
@@ -15,26 +14,32 @@ export class MainPageFacade {
   private readonly tarotService = inject(TarotService);
   private readonly myMemoryTranslationService = inject(MyMemoryTranslationService);
   private readonly translocoService = inject(TranslocoService);
-  private readonly destroyRef = inject(DestroyRef);
-  private readonly _error = signal<unknown>(null);
-  private readonly _isTarotLoading = signal(false);
-  private readonly _isTranslationLoading = signal(false);
+  private readonly isTranslationLoading = signal(false);
   private readonly _result = signal<TarotResponseApi | null>(null);
-  private readonly _sourceResult = linkedSignal<TarotResponseApi | null>(() => {
+  private readonly sourceResult = linkedSignal<TarotResponseApi | null>(() => {
     this.role();
     this.intent();
 
     return null;
   });
+  private readonly useRxResource = rxResource({
+    params: () => ({
+      role: this.role,
+      intent: this.intent,
+    }),
+
+    stream: ({ params: { intent, role } }) => this.tarotService.loadReading(role(), intent()),
+  });
+
   public readonly result = this._result.asReadonly();
-  public readonly isLoading = computed(() => this._isTarotLoading() || this._isTranslationLoading());
+  public readonly isLoading = computed(() => this.useRxResource.isLoading() || this.isTranslationLoading());
   public intent = this.tarotService.intent;
   public role = this.tarotService.role;
-  public readonly error = this._error.asReadonly();
+  public readonly error = this.useRxResource.error;
 
   constructor() {
     effect((onCleanup) => {
-      const sourceResult = this._sourceResult();
+      const sourceResult = this.sourceResult();
       const activeLang = this.translocoService.activeLang() as Languages;
 
       if (!sourceResult) {
@@ -43,11 +48,11 @@ export class MainPageFacade {
         return;
       }
 
-      this._isTranslationLoading.set(true);
+      this.isTranslationLoading.set(true);
 
       const subscription = this.myMemoryTranslationService
         .translateReading(sourceResult, activeLang)
-        .pipe(finalize(() => this._isTranslationLoading.set(false)))
+        .pipe(finalize(() => this.isTranslationLoading.set(false)))
         .subscribe({
           next: (translatedResult) => {
             this._result.set(translatedResult);
@@ -64,29 +69,8 @@ export class MainPageFacade {
   }
 
   public loadTarot(): void {
-    this._isTarotLoading.set(true);
-    this._error.set(null);
-
-    this.tarotService
-      .loadReading()
-      .pipe(
-        finalize(() => {
-          this._isTarotLoading.set(false);
-        }),
-        takeUntilDestroyed(this.destroyRef)
-      )
-      .subscribe({
-        next: (tarotResponse) => {
-          this._sourceResult.set(tarotResponse);
-        },
-        error: (error: unknown) => {
-          if (error instanceof HttpErrorResponse) {
-            this._error.set(error.message);
-
-            return;
-          }
-          this._error.set('Unknown error');
-        },
-      });
+    if (this.useRxResource.hasValue()) {
+      this.sourceResult.set(this.useRxResource.value());
+    }
   }
 }
